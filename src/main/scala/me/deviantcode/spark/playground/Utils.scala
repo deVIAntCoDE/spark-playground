be1a3500ed9c83b3f0d1f3d5bec57041455522f0
deviantcode <hnocleland@gmail.com>
2018-05-25 13:11:55 +0100

    Re add Dataframes & Datasets from

--- src/main/scala/me/deviantcode/spark/playground/Utils.scala
+++ src/main/scala/me/deviantcode/spark/playground/Utils.scala
@@ -6,6 +6,7 @@ object Utils {
 
   /*
   Creating spark sessions
+  give it optional values for partitions and shuffles to indicate local mode runs
    */
   def getSparkSession(par: Option[Int] = None, shuffleParts: Option[Int] = None): SparkSession = {
     val spark = par.map(cores => {
@@ -24,4 +25,8 @@ object Utils {
     shuffleParts.foreach(spark.conf.set("spark.sql.shuffle.partitions", _)) // reduce shuffle partitions to reduce network overhead in localmode
     spark
   }
+
+  //calculate minuted from midnight, input is military time format
+  def getMinuteOfDay(depTime: String): Int = (depTime.toInt / 100).toInt * 60 + (depTime.toInt % 100)
+
 }
